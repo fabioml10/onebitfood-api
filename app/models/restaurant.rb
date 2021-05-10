95ee8659cbbba3544f9dce7ad37920c018c7c3de
Fabio Muller <fabioml10@yahoo.com.br>
2021-05-10 09:48:08 -0300

    Configure active storage

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -3,5 +3,7 @@ class Restaurant < ApplicationRecord
   has_many :product_categories
   has_many :orders
 
+  has_one_attached :image
+
   validates :name, :status, :delivery_tax, :city, :neighborhood, :street, :number, presence: true
 end
